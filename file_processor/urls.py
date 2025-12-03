7a01ebd888c8b3cb16548c2f6957ceb1da2a3659
Fran Wu <fran.wu@huhtamaki.com>
2025-12-03 14:03:18 +0800

    Move admin.py and views.py to file_processor directory and update imports

--- file_processor/urls.py
+++ file_processor/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+
+
+    
+    # Authentication URLs
+    path('auth/login/', auth_views.LoginView.as_view(), name='login'),
+    path('auth/logout/', auth_views.LogoutView.as_view(next_page='/auth/login/'), name='logout'),
+
+    
+
     path('', include('file_processor.file.urls')),
 ]
